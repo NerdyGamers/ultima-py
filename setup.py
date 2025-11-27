ff3a55fde314104337a97ef330444991bcc18475
JBob <91869208+NerdyGamers@users.noreply.github.com>
2025-11-26 21:40:42 -0500

    Modernize packaging: Add pyproject.toml, update dependencies, add CI/CD

--- setup.py
+++ setup.py
@@ -1,25 +1,5 @@
+# Legacy setup.py for backwards compatibility
+# All configuration is now in pyproject.toml
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="ultimapy",
-    version="0.0.24",
-    author="Jack Ward",
-    author_email="jackward84@gmail.com",
-    description="UltimaPy - Extract information and images from the UO client files",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jackuoll/ultima-py",
-    packages=setuptools.find_packages(),
-    license="Beerware",
-    install_requires=[
-        'Pillow',
-        'imageio'
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-)
+setuptools.setup()
